88241d017d6eea6961febb728d4221c2d96bc271
Dms Fahim Patoawary <dmsfahim@gmail.com>
2023-02-06 11:45:41 +0600

    06-02-23

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,3 +57,54 @@ CatagorisIcinHideCros2222.addEventListener("click", function() {
     document.getElementById("CatagorisIcinHideCros").style.display = "none";
 });
 
+// order page
+let ProceedToCheckout1 = document.getElementById("ProceedToCheckout");
+function ProceedToCheckoutFuns() {
+    document.getElementById("cartTab").style.display = "none";
+    document.getElementById("checkOutTab").style.display = "block";
+};
+
+// order page
+let PalceOrder1 = document.querySelector(".PalceOrder");
+PalceOrder1.addEventListener("click", function() {
+    document.getElementById("checkOutTab").style.display = "none";
+    document.getElementById("orderCompletedTab").style.display = "block";
+});
+
+ // login page hide show
+ let registBtn = document.getElementById("RegistBtnLogin");
+ function loginSing(){
+     let LoginDiv = document.querySelector(".LoginDiv");
+     let SingDiv = document.querySelector(".SingDiv");
+     LoginDiv.style.display = "none";
+     SingDiv.style.display = "block";
+ }
+
+ // singin
+ let LoginBtn = document.querySelector(".LoginBtn");
+ function loginSingup(){
+     let LoginDiv = document.querySelector(".LoginDiv");
+     let SingDiv = document.querySelector(".SingDiv");
+     let ForgotBtn = document.querySelector(".ForgotPassowrdDiv");
+     LoginDiv.style.display = "block";
+     SingDiv.style.display = "none";
+     ForgotBtn.style.display = "none";
+ }
+ 
+ // Forgot
+ let ForgotBtn = document.querySelector(".ForGotBtn");
+ function prgothide(){
+     let LoginDiv = document.querySelector(".LoginDiv");
+     let ForgotBtn = document.querySelector(".ForgotPassowrdDiv");
+     LoginDiv.style.display = "block";
+     ForgotBtn.style.display = "none";
+ }
+ 
+ // Forgot
+ let ForgotShowcli = document.querySelector(".ForgotShowcli");
+ function ForgotShow(){
+     let LoginDiv = document.querySelector(".LoginDiv");
+     let ForgotBtn = document.querySelector(".ForgotPassowrdDiv");
+     LoginDiv.style.display = "none";
+     ForgotBtn.style.display = "block";
+ }
